051b3afe18b110c373d216c937f8d83af6d84e18
Naga Chaitanya Konada <chaitu.k.nag@gmail.com>
2019-10-10 09:46:49 +0000

    req.ip and req.ips

--- express-ip-detect-demo/server.js
+++ express-ip-detect-demo/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express();
 
+app.set('trust proxy', true);
+
 app.get('/', (req, res) => {
     console.log(req.headers['x-forwarded-for'], req.connection.remoteAddress);
+    console.log(req.ip, req.ips);
     res.status(200).end();
 });
 
